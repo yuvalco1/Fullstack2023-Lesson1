58a3e13f135617cdeb3c5eece6a7354cbdd95ff2
Yuval Cohen <yuvalco1@gmail.com>
2023-09-20 17:42:56 +0300

    Lesson2 first exercise

--- Lesson2/lesson2/src/Main.java
+++ Lesson2/lesson2/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Create 3 variables for first name, last name, age and print them
+
+        String firstName = "Yuval";
+        String lastName = "Cohen";
+        int age = 53;
+
+        System.out.println(firstName+" "+lastName+ " age is:"+ age );
     }
 }
