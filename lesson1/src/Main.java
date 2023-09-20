7505afa15ee6fc04ff62855a850abe7601ef6954
Yuval Cohen <yuvalco1@gmail.com>
2023-09-20 08:15:08 +0300

    Lesson1 first app

---
+++ lesson1/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+        int a = 5;
+        int b = 10;
+        System.out.println("a is:" + a + " and b is:" + b);
+        System.out.println("Now switching values between a & b");
+//        switch values with 3rd argument
+//        int c=b;
+//        b=a;
+//        a=c;
+
+        // Switch values without 3rd argument
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("Now a is:" + a + " and b is:" + b);
+        System.out.println();
+
+    }
+}
